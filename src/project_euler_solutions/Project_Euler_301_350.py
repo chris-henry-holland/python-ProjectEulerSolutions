d838e3895709078c8b7cbb7179b3de4421931404
chris-henry-holland <chris.henry.holland@gmail.com>
2026-04-11 19:21:34 +0100

    Added provisional solution to Project Euler #306. Not yet verified

--- src/project_euler_solutions/Project_Euler_301_350.py
+++ src/project_euler_solutions/Project_Euler_301_350.py
@@ -48,6 +48,31 @@ def nimVariantPlayer2WinsWithPerfectPlayConfigurationsCount(pow2: int=30) -> int
         curr = [curr[1], sum(curr)]
     return curr[1]
 
+
+
+# Problem 306
+def paperStripGamePlayer1WinsWithPerfectPlayCount(n_square_pick: int=2, n_max: int=10 ** 6) -> int:
+    """
+    Solution to Project Euler #306
+    """
+    # Using Sprague-Grundy theorem
+    g_arr = [0] * n_square_pick
+    res = 0
+    for i in range(n_square_pick, n_max + 1):
+        if not i % 1000: print(f"i = {i} of {n_max}")
+        seen = set()
+        for i2 in range(0, ((i - n_square_pick) >> 1) + 1):
+            g = g_arr[i2] ^ g_arr[i - i2 - n_square_pick]
+            seen.add(g)
+        for g in range(len(seen) + 1):
+            if g in seen: continue
+            g_arr.append(g)
+            break
+        res += bool(g_arr[-1])
+    return res
+
+
+
 ##############
 project_euler_num_range = (301, 350)
 
@@ -63,8 +88,13 @@ def evaluateProjectEulerSolutions251to300(eval_nums: Optional[Set[int]]=None) ->
         print(f"Solution to Project Euler #301 = {res}, calculated in {time.time() - since:.4f} seconds")
 
 
+    if 306 in eval_nums:
+        since = time.time()
+        res = paperStripGamePlayer1WinsWithPerfectPlayCount(n_square_pick=2, n_max=10 ** 5)
+        print(f"Solution to Project Euler #301 = {res}, calculated in {time.time() - since:.4f} seconds")
+
     print(f"Total time taken = {time.time() - since0:.4f} seconds")
 
 if __name__ == "__main__":
-    eval_nums = {301}
+    eval_nums = {306}
     evaluateProjectEulerSolutions251to300(eval_nums)
