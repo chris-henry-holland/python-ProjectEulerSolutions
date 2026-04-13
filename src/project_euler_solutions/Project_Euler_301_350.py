da53a74faac6733b9d9f35eb0134f799c10d7838
chris-henry-holland <chris.henry.holland@gmail.com>
2026-04-13 16:12:20 +0100

    Added solution to Project Euler #304. Needs documentation and outline of rationale.

--- src/project_euler_solutions/Project_Euler_301_350.py
+++ src/project_euler_solutions/Project_Euler_301_350.py
@@ -48,6 +48,125 @@ def nimVariantPlayer2WinsWithPerfectPlayConfigurationsCount(pow2: int=30) -> int
         curr = [curr[1], sum(curr)]
     return curr[1]
 
+# Problem 304
+def calculatePrimeFactorisation(
+    num: int,
+    ps: Optional[PrimeSPFsieve]=None,
+) -> Dict[int, int]:
+    """
+    For a strictly positive integer, calculates its prime
+    factorisation.
+
+    This is performed using direct division.
+
+    Args:
+        Required positional:
+        num (int): The strictly positive integer whose prime
+                factorisation is to be calculated.
+
+        Optional named:
+        ps (PrimeSPFsieve object or None): If given, a smallest
+                prime factor prime sieve object used to calculate
+                the prime factorisation. If given as None, the
+                factorisation is performed through direct division.
+    
+    Returns:
+    Dictionary (dict) giving the prime factorisation of num, whose
+    keys are strictly positive integers (int) giving the prime
+    numbers that appear in the prime factorisation of num, with the
+    corresponding value being a strictly positive integer (int)
+    giving the number of times that prime appears in the
+    factorisation (i.e. the power of that prime in the prime
+    factorisation of the factor num). An empty dictionary is
+    returned if and only if num is the multiplicative identity
+    (i.e. 1).
+    """
+    if ps is not None:
+        return ps.primeFactorisation(num)
+    exp = 0
+    while not num & 1:
+        num >>= 1
+        exp += 1
+    res = {2: exp} if exp else {}
+    for p in range(3, num, 2):
+        if p ** 2 > num: break
+        exp = 0
+        while not num % p:
+            num //= p
+            exp += 1
+        if exp: res[p] = exp
+    if num > 1:
+        res[num] = 1
+    return res
+
+def primonacciSum(
+    n_max: int=10 ** 5,
+    prime_start: int=10 ** 14,
+    res_md: Optional[int]=1234567891011,
+) -> int:
+    """
+    Solution to Project Euler #304
+    """
+    # Note: res_md must be odd
+
+    # Review- look into solutions utilising Pisano periods
+
+    ps = SimplePrimeSieve()
+    def primeCheck(num: int) -> int:
+        return ps.millerRabinPrimalityTestWithKnownBounds(num, max_n_additional_trials_if_above_max=10)[0]
+    
+    def nextPrime(num: int) -> int:
+        if num < 2: return 2
+        start = num + 1 + (num & 1)
+        for num2 in itertools.count(start, step=2):
+            if primeCheck(num2): return num2
+        return -1
+    
+    addMod = (lambda a, b: a + b) if res_md is None else (lambda a, b: (a + b) % res_md)
+    multMod = (lambda a, b: a * b) if res_md is None else (lambda a, b: (a * b) % res_md)
+
+    res_md_totient = 1
+    if res_md is not None:
+        pf = calculatePrimeFactorisation(res_md)
+        for p, f in pf.items():
+            res_md_totient *= p ** (f - 1) * (p - 1)
+    
+    def calculateFibonacci(n: int) -> int:
+        # Using Binet's formula
+        #print(n)
+        curr1 = [1, 0]
+        curr2 = [1, 0]
+        exp1 = [1, 1]
+        exp2 = [1, -1] if res_md is None else [1, res_md - 1]
+        n2 = n
+        while n2:
+            #print(n2)
+            if n2 & 1:
+                #print("hi")
+                curr1 = [addMod(multMod(curr1[0], exp1[0]), multMod(5, multMod(curr1[1], exp1[1]))), addMod(multMod(curr1[0], exp1[1]), multMod(curr1[1], exp1[0]))]
+                curr2 = [addMod(multMod(curr2[0], exp2[0]), multMod(5, multMod(curr2[1], exp2[1]))), addMod(multMod(curr2[0], exp2[1]), multMod(curr2[1], exp2[0]))]
+            n2 >>= 1
+            exp1 = [addMod(multMod(exp1[0], exp1[0]), multMod(5, multMod(exp1[1], exp1[1]))), multMod(multMod(exp1[0], exp1[1]), 2)]
+            exp2 = [addMod(multMod(exp2[0], exp2[0]), multMod(5, multMod(exp2[1], exp2[1]))), multMod(multMod(exp2[0], exp2[1]), 2)]
+        #print(curr1, curr2)
+        #print(exp1, exp2)
+        res = addMod(curr1[1], -curr2[1])
+        if res_md is None:
+            return res >> n
+        inv2 = pow(2, res_md_totient - 1, res_md)
+        #print(f"inv2 = {inv2}, (inv2 * 2) % res_md = {(inv2 * 2) % res_md}")
+        return multMod(res, pow(inv2, n, res_md))
+    
+    curr = prime_start
+    res = 0
+    for _ in range(n_max):
+        curr = nextPrime(curr)
+        #print(f"curr = {curr}")
+        fib = calculateFibonacci(curr)
+        res = addMod(res, fib)
+        #print(curr, fib)
+        #print(f"res = {res}")
+    return res
 
 
 # Problem 306
@@ -121,6 +240,15 @@ def evaluateProjectEulerSolutions251to300(eval_nums: Optional[Set[int]]=None) ->
         print(f"Solution to Project Euler #301 = {res}, calculated in {time.time() - since:.4f} seconds")
 
 
+    if 304 in eval_nums:
+        since = time.time()
+        res = primonacciSum(
+            n_max=10 ** 5,
+            prime_start=10 ** 14,
+            res_md=1234567891011,
+        )
+        print(f"Solution to Project Euler #304 = {res}, calculated in {time.time() - since:.4f} seconds")
+
     if 306 in eval_nums:
         since = time.time()
         res = paperStripGamePlayer1WinsWithPerfectPlayCount(n_max=10 ** 6)
@@ -129,5 +257,5 @@ def evaluateProjectEulerSolutions251to300(eval_nums: Optional[Set[int]]=None) ->
     print(f"Total time taken = {time.time() - since0:.4f} seconds")
 
 if __name__ == "__main__":
-    eval_nums = {306}
+    eval_nums = {304}
     evaluateProjectEulerSolutions251to300(eval_nums)
