21930e957bae67f23350fa6d1434483475db0514
chris-henry-holland <chris.henry.holland@gmail.com>
2026-08-14 21:22:36 +0100

    Added solution to Project Euler #345. Needs documentation and outline of rationale.

--- src/project_euler_solutions/Project_Euler_301_350.py
+++ src/project_euler_solutions/Project_Euler_301_350.py
@@ -4808,6 +4808,9 @@ def spillBeansMoveCountForFloorHalfXorSequence(
     floor_hlf_xor_seq_term_md: int = 1 << 11,
     floor_hlf_xor_seq_term_offset: int=1,
 ) -> int:
+    """
+    Solution to Project Euler #334
+    """
     seq = (
         x[0] for x in zip(
             floorHalfXorSequenceGenerator(
@@ -5743,6 +5746,47 @@ def cubeFractionalSequenceEndSum(
         res += ans
     return res
 
+# Problem 345
+def maxMatrixSum(mat: list[list[int]]) -> int:
+    shape = (len(mat), len(mat[0]))
+    curr_bms = {0: 0}
+    for i1 in reversed(range(shape[0])):
+        prev_bms = dict(curr_bms)
+        for bm, score in prev_bms.items():
+            bm2 = ((1 << shape[1]) - 1) ^ bm
+            while bm2:
+                lo = bm2 & (-bm2)
+                i2 = lo.bit_length() - 1
+                bm3 = bm | lo
+                curr_bms[bm3] = max(curr_bms.get(bm3, -float("inf")), score + mat[i1][i2])
+                bm2 ^= lo
+    return max(curr_bms.values())
+
+def getProjectEulerProblem345Matrix() -> list[list[int]]:
+    return [
+        [7, 53, 183, 439, 863, 497, 383, 563, 79, 973, 287, 63, 343, 169, 583],
+        [627, 343, 773, 959, 943, 767, 473, 103, 699, 303, 957, 703, 583, 639, 913],
+        [447, 283, 463, 29, 23, 487, 463, 993, 119, 883, 327, 493, 423, 159, 743],
+        [217, 623, 3, 399, 853, 407, 103, 983, 89, 463, 290, 516, 212, 462, 350],
+        [960, 376, 682, 962, 300, 780, 486, 502, 912, 800, 250, 346, 172, 812, 350],
+        [870, 456, 192, 162, 593, 473, 915, 45, 989, 873, 823, 965, 425, 329, 803],
+        [973, 965, 905, 919, 133, 673, 665, 235, 509, 613, 673, 815, 165, 992, 326],
+        [322, 148, 972, 962, 286, 255, 941, 541, 265, 323, 925, 281, 601, 95, 973],
+        [445, 721, 11, 525, 473, 65, 511, 164, 138, 672, 18, 428, 154, 448, 848],
+        [414, 456, 310, 312, 798, 104, 566, 520, 302, 248, 694, 976, 430, 392, 198],
+        [184, 829, 373, 181, 631, 101, 969, 613, 840, 740, 778, 458, 284, 760, 390],
+        [821, 461, 843, 513, 17, 901, 711, 993, 293, 157, 274, 94, 192, 156, 574],
+        [34, 124, 4, 878, 450, 476, 712, 914, 838, 669, 875, 299, 823, 329, 699],
+        [815, 559, 813, 459, 522, 788, 168, 586, 966, 232, 308, 833, 251, 631, 107],
+        [813, 883, 451, 509, 615, 77, 281, 613, 459, 205, 380, 274, 302, 35, 805],
+    ]
+
+def maxMatrixSumForProjectEulerProblem345Matrix() -> int:
+    """
+    Solution to Project Euler #345
+    """
+    return maxMatrixSum(getProjectEulerProblem345Matrix())
+
 ##############
 project_euler_num_range = (301, 350)
 
@@ -6060,10 +6104,15 @@ def evaluateProjectEulerSolutions251to300(eval_nums: Optional[Set[int]]=None) ->
         )
         print(f"Solution to Project Euler #343 = {res}, calculated in {time.time() - since:.4f} seconds")
 
+    if 345 in eval_nums:
+        since = time.time()
+        res = maxMatrixSumForProjectEulerProblem345Matrix()
+        print(f"Solution to Project Euler #345 = {res}, calculated in {time.time() - since:.4f} seconds")
+
     print(f"Total time taken = {time.time() - since0:.4f} seconds")
 
 if __name__ == "__main__":
-    eval_nums = {334}
+    eval_nums = {345}
     evaluateProjectEulerSolutions251to300(eval_nums)
 
 
@@ -6235,3 +6284,16 @@ for n in range(10 ** 12, 10 ** 12 + 1):
 #    print(n, fractionalSequenceEnd(n, ps=SimplePrimeSieve()))
 
 #print(spillBeansMoveCount([2, 3]))
+"""
+print(
+    maxMatrixSum(
+        [
+            [7, 53, 183, 439, 863],
+            [497, 383, 563, 79, 973],
+            [287, 63, 343, 169, 583],
+            [627, 343, 773, 959, 943],
+            [767, 473, 103, 699, 303],
+        ]
+    )
+)
+"""
